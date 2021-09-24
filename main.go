3dd1a4913288721f65756fba51350c603d0facea
EliodorM <tanase.eliodor.madalin@gmail.com>
2021-09-24 13:24:36 +0300

    create db

--- main.go
+++ main.go
@@ -1,6 +1,43 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "admin"
+	password = "admin"
+	dbname   = "myPostgreDB"
+)
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user, password)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	createDB(db, dbname)
+	err = db.Close()
+	if err != nil {
+		panic(err)
+
+	}
+	db.Close()
+}
+
+func createDB(db *sql.DB, name string) error {
+	_, err := db.Exec("CREATE DATABASE " + name)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func normalize(phone string) string {
 	var buf bytes.Buffer
